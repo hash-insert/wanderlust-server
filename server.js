a6541dd8b85617847cfdfbc9d51d5adc6e1d9b8f
uma3476 <umanaidu347@gmail.com>
2022-09-22 15:18:28 +0530

    wanderlust done

--- server.js
+++ server.js
@@ -12,3 +12,19 @@ app.get('/countries', (req, res) => {
 })
 
 app.listen(5000, () => console.log('Listening on port 5000 ...'))
+
+
+app.get('/country/id/:abc', (req, res) => {
+    console.log(req.params);
+    console.log(req.query);
+    const wlcountry = country.filter((item) => item.id == req.params.abc)
+    if (wlcountry.length === 0) res.status(404).send('Country not found for id ' + req.params.abc);
+    res.send(JSON.stringify(wlcountry));
+})
+app.get('/country/city/:xyz', (req, res) => {
+    console.log(req.params);
+    console.log(req.query);
+    const wlcountry = country.filter((item) => item.city.toLowerCase() === req.params.xyz.toLowerCase())
+    if (wlcountry.length === 0) res.status(404).send('Country not for city ' + req.params.xyz);
+    res.send(JSON.stringify(wlcountry));
+})
