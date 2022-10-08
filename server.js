5e6a5737b5a605d4e95bbcaaace401816cd597da
kaushiksaraf <kaushiksaraf91@gmail.com>
2022-10-08 15:36:28 +0530

    wanderlust revamed with rest apis done

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const { request, response } = require('express')
+
 const express = require ('express')
-const require =require ('request')
+
 const app =express()
 
-request('',(erroe,response,body)=>{
-    
-})
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+
 
 const countries=[{
     id:1,
@@ -67,6 +67,61 @@ app.get('/countries/:id', (req, res) => {
     if (countryById.length === 0) res.status(404).send('country not found');
     res.send(JSON.stringify(countryById));
 })
+app.get('/places/:place', (req, res) => {
+   
+    console.log(req.params);
+    
+    const countryByPlaces = countries.filter((item) => item.place.toLowerCase() == req.params.place.toLowerCase())
+
+    
+    if (countryByPlaces.length === 0) res.status(404).send('place not found');
+    res.send(JSON.stringify(countryByPlaces));
+})
+
+
+app.post('/places',(req,res)=> {
+ console.log(req.body)
+ const{country,place}=req.body
+    let addplace={
+        id: countries.length + 1,
+       country:req.body.country,
+        place:req.body.place
+    }
+
+    console.log(addplace)
+    
+    countries.push(addplace)
+    console.log(countries)
+    res.redirect('/countries')
+})
+
+app.put('/places/:id',(req,res)=> {
+    const id = req.params
+    const getId= countries.find((item) => item.id == req.params.id);
+ if(!id){
+        res.send('Provide some id to update')
+        return
+    }
+
+    getId.place=req.body.place
+
+    res.redirect('/countries')   
+})
+
+
+app.delete('/places/:id',(req,res)=> {
+    const id = req.params
+    const getId= countries.find((item) => item.id == req.params.id);
+ if(!id){
+        res.send('Provide some id to update')
+        return
+    }
+
+    const idx = countries.indexOf(getId)
+    countries.splice(idx,1)
+
+    res.redirect('/countries')  
+})
 
 
 const port=process.env.PORT || 8000;
@@ -92,3 +147,17 @@ app.listen(port, () => console.log(`Listening on port ${port} ...`));
 //     res.send(JSON.stringify(course));
 // })
 
+// app.get('/country/id/:abc', (req, res) => {
+//     console.log(req.params);
+//     console.log(req.query);
+//     const wlcountry = country.filter((item) => item.id == req.params.abc)
+//     if (wlcountry.length === 0) res.status(404).send('Country not found for id ' + req.params.abc);
+//     res.send(JSON.stringify(wlcountry));
+// })
+// app.get('/country/city/:xyz', (req, res) => {
+//     console.log(req.params);
+//     console.log(req.query);
+//     const wlcountry = country.filter((item) => item.city.toLowerCase() === req.params.xyz.toLowerCase())
+//     if (wlcountry.length === 0) res.status(404).send('Country not for city ' + req.params.xyz);
+//     res.send(JSON.stringify(wlcountry));
+// })
